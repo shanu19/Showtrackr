ab327d43328f94ad468e3cad2e561b2de1eb9fce
shanu19 <jshanuja@gmail.com>
2018-09-25 17:23:33 +0530

    ups

--- app/routes.js
+++ app/routes.js
@@ -44,7 +44,7 @@ module.exports = function(app, passport) {
     app.post('/signup', passport.authenticate('local-signup', {
         successRedirect : '/', // redirect to the secure profile section
         failureRedirect : '/signup', // redirect back to the signup page if there is an error
-        failureFlash : true // allow flash messages
+        // failureFlash : true // allow flash messages
     }));
 
     // =====================================
